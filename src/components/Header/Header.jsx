3dfd42e75689543bf8a63129c2baf8acf38fa57a
abu-jafor <abujaforwd@yahoo.com>
2023-04-01 20:20:25 +0600

    img path correction

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,9 +9,12 @@ const Header = () => {
         </div>
         <div className="flex-none gap-2">
           <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img src="../../../src/img/test.jpg" />
+            <label
+              tabIndex={0}
+              className="btn btn-ghost btn-circle avatar me-5"
+            >
+              <div className="w-20 rounded-full">
+                <img src="https://media.istockphoto.com/id/1399565382/photo/young-happy-mixed-race-businessman-standing-with-his-arms-crossed-working-alone-in-an-office.jpg?s=612x612&w=is&k=20&c=SRtmXotZfERsp4_0tTn24AXdCTWyssWO8r6dj5ehI98=" />
               </div>
             </label>
           </div>
